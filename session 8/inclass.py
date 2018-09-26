adf92a62766e97eac9b34ecf7cfc1c2aad5d19f5
ZiruiJ <32169241+ZiruiJ@users.noreply.github.com>
2018-09-25 22:24:10 -0400

    publish inclass session8

---
+++ session 8/inclass.py
@@ -0,0 +1,9 @@
+print ('bananas',   chr (36), 52)
+print ('rice',  chr (36), 35)
+print ('paprika',   chr (36), 72)
+print ('potato chips',  chr (36), 78)
+print ('-------------')
+print ('total', chr (36), 237)
+
+for line in [['bananas',chr (36), 52.00], ['rice',chr(36), 35.00],['paprika', chr(36), 72.00], ['potato chips', chr (36), 78.00], ['total', chr(36), 237.00]]:
+    print('{:<20} {:<2} {:<20.2f} '.format(*line))
--- session 8/test.py
+++ session 8/test.py
@@ -1,4 +1,4 @@
-team= "new england patriots"
+# team= "New england patriots"
 
 # def find(word, letter):
 #     index=0
@@ -10,9 +10,40 @@ team= "new england patriots"
 
 # print (find (team, 'e'))
 
-count=0
-for letter in team:
-    if letter =='a':
-        count= count+1
+# count=0
+# for letter in team:
+#     if letter =='a':
+#         count= count+1
 
-print (count)
+# print (count)
+
+
+'''Encapsulate this code in a function named count, and generalize it so that 
+it accepts the string and the letter as arguments.'''
+
+# def count (s, letter):
+#     count= 0
+#     for each in s:
+#         if each == letter:
+#             count= count+1
+#     return count
+
+# print (count (team, 'n'))
+
+# new_team= team.upper()
+# print (new_team)
+
+# n= team.find('a')
+# # print (n )
+# print(team.find('en'))
+
+# print(team.find('a', 10))
+'''question print(team.find('a', 10)) 
+# Read the documentation of this function to find out
+# what it does.
+# https://docs.python.org/3/library/stdtypes.html#str.find
+# find(sub[, start[, end]])'''
+
+name = 'bob'
+print(name.find('b', 1, 3))
+
